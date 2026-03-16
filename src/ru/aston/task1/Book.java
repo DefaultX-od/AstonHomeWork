a39a51fb4410d89276c7c50c0ec12efd3355646e
defaultxod <defaultx-od@yandex.ru>
2026-03-17 01:35:55 +0300

    Task1 fixed and completed

---
+++ src/ru/aston/task1/Book.java
@@ -0,0 +1,50 @@
+package ru.aston.task1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public final class Book {
+    private final String name;
+    private final String author;
+    private final List<Page> pages;
+    private final int pageCount;
+
+    public Book(String name, String author, List<Page> pages){
+        this.name = name == null ? "" : name;
+        this.author = author == null ? "" : author;
+        this.pages = new ArrayList<>();
+
+        if(pages != null) {
+            for (Page page : pages) {
+                if(page != null) this.pages.add(page.clone());
+            }
+        }
+        this.pageCount = this.pages.size();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public List<Page> getPages() {
+        List<Page> pagesCopy = new ArrayList<>();
+        for (Page page: this.pages){
+            pagesCopy.add(page.clone());
+        }
+        return List.copyOf(pagesCopy);
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("| Название: %s| Автор: %s | Количество страниц %d |",this.name, this.author, this.pageCount);
+    }
+}
---
+++ src/ru/aston/task1/Page.java
@@ -0,0 +1,60 @@
+package ru.aston.task1;
+
+public class Page implements Cloneable{
+    private int id;
+    private int wordCount;
+    private String content;
+
+    public Page(int id, String content){
+        this.id = id;
+        this.content = content;
+        this.wordCount = this.countWords();
+    }
+
+    protected Page(int id, int wordCount, String content){
+        this.id = id;
+        this.content = content;
+        this.wordCount = wordCount;
+    }
+
+    public int countWords(){
+        if(this.content.isEmpty()) return 0;
+        return this.content.split(" ").length;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+        this.wordCount = this.countWords();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getWordCount() {
+        return wordCount;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("| Страница: %d | Количество слов: %d | Содержание: %s |", this.id, this.wordCount, this.content);
+    }
+
+    @Override
+    public Page clone(){
+        try {
+            return (Page) super.clone();
+        }
+        catch (CloneNotSupportedException e){
+            throw new AssertionError(e);
+        }
+    }
+}
