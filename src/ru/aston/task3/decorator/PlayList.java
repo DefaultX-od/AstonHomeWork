629b8b47fbb691e32a2b3a91ef7cd73b09b6187f
defaultxod <defaultx-od@yandex.ru>
2026-03-29 08:45:02 +0300

    Task 3 update:
    1) Added decorator implementation

---
+++ src/ru/aston/task3/decorator/PlayList.java
@@ -0,0 +1,35 @@
+package ru.aston.task3.decorator;
+
+public final class PlayList implements IPlayList {
+    private final Integer id;
+    private final String name;
+    private final String imageCoverLink;
+
+    public PlayList(final Integer id, final String name, final String imageCoverLink) {
+        this.id = id;
+        this.name = name;
+        this.imageCoverLink = imageCoverLink;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImageCoverLink() {
+        return imageCoverLink;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("id: %d; name: %s; imageCoverLink: %s;", id, name, imageCoverLink);
+    }
+
+    @Override
+    public String showInfo() {
+        return this.toString();
+    }
+}
